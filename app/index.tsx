f7b32feb122de62014b2df52cfd5692f88dca7f7
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-09-25 10:54:22 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl font-bold text-dark-200">Welcome!</Text>
+      <Link href="/onboard">Onboard</Link>
+    </View>
+  );
+}
